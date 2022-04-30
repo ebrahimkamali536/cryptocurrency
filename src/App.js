ad6e53a53e8854e314acb57d313d4fa506e58923
ebrahim kamali <ebrahimkamali536@gmail.com>
2022-04-30 16:29:36 +0430

    create context and wrap to app, context data: currency and symbol

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 //components
-import Header from './components/Header';
-import CoinPage from './pages/CoinPage';
-import HomePage from './pages/HomePage';
+import Header from "./components/Header";
+import CoinPage from "./pages/CoinPage";
+import HomePage from "./pages/HomePage";
 
+//context
+import CryptoContextProvider from "./context/CryptoContextProvider";
 const App = () => {
   return (
-    <div className='dark:bg-slate-900 min-h-screen'>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/coins/:id' element={<CoinPage />} />
-      </Routes>
-    </div>
+    <CryptoContextProvider>
+      <div className="dark:bg-slate-900 min-h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </CryptoContextProvider>
   );
 };
 
---
+++ src/context/CryptoContextProvider.js
@@ -0,0 +1,18 @@
+import React, { createContext, useEffect, useState } from 'react';
+export const CryptoContext = createContext()
+const CryptoContextProvider = ({children}) => {
+    const [currency, setCurrency] = useState("USD");
+    const [symbol, setSymbol] = useState("$");
+
+    useEffect(() => {
+        if(currency === "USD") setSymbol("$")
+        else if (currency === "INR") setSymbol("₹")
+    }, [currency])
+    return (
+        <CryptoContext.Provider value={{currency, symbol, setCurrency}}>
+            {children}
+        </CryptoContext.Provider>
+    );
+};
+
+export default CryptoContextProvider;
